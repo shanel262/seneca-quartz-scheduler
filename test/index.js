c9117fa369b3ef40acce3dd7036f2b516fdd4a5e
Guy Ellis <wildfiction@gmail.com>
2014-09-18 14:45:02 -0700

    Added deregister micro service

--- test/index.js
+++ test/index.js
@@ -1,9 +1,16 @@
 var seneca = require('seneca')();
 var moment = require('moment');
 var expect = require('chai').expect;
+var debug = require('debug')('test:seneca-quartz-scheduler');
+
+var config = {
+  // When testing, change the URL to where your scheduler is running
+  quartzURL: 'http://127.0.0.1:8090/scheduler/api'
+};
 
 var role = 'scheduler';
-seneca.use('../lib/scheduler.js');
+
+seneca.use('../lib/scheduler.js', config);
 
 it('should register a task', function (done) {
   var registeredAt;
@@ -12,7 +19,7 @@ it('should register a task', function (done) {
     seneca.act({
       role: role,
       cmd: 'register',
-      when: moment().add('seconds', 1).toDate(),
+      when: moment().add(1, 'seconds').toDate(),
       task: function () {
         var now = Date.now();
         expect(now).to.be.above(registeredAt);
