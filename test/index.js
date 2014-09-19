6f74d9e007c80ac70e2ca1f2017cd07a2937f46e
Guy Ellis <wildfiction@gmail.com>
2014-09-19 15:11:56 -0700

    Added tests and update cmd

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 var seneca = require('seneca')();
 var moment = require('moment');
 var expect = require('chai').expect;
+var should = require('chai').should();
 var debug = require('debug')('test:seneca-quartz-scheduler');
 
 var config = {
@@ -12,22 +13,124 @@ var role = 'scheduler';
 
 seneca.use('../lib/scheduler.js', config);
 
-it('should register a task', function (done) {
-  var registeredAt;
+describe('quartz events', function(){
 
-  seneca.ready(function() {
-    seneca.act({
-      role: role,
-      cmd: 'register',
-      when: moment().add(1, 'seconds').toDate(),
-      task: function () {
-        var now = Date.now();
-        expect(now).to.be.above(registeredAt);
+  it('should register a task and have it execute a second later', function (done) {
+    var registeredAt;
+
+    seneca.ready(function () {
+      seneca.act({
+        role: role,
+        cmd: 'register',
+        when: moment().add(1, 'seconds').toDate(),
+        task: function (data) {
+          debug('task executed:');
+          debug(data);
+          var now = Date.now();
+          expect(now).to.be.above(registeredAt);
+          done();
+        },
+        random: {gold: 'color'}
+      }, function (err, data) {
+        debug('Registered task:');
+        debug(data);
+        registeredAt = Date.now();
+        expect(err).to.be.null;
+      });
+    });
+  });
+
+  var taskData;
+  var delayedTaskExecuted = false;
+  var delayedTask = function() {
+    delayedTaskExecuted = true;
+  };
+
+  it('should register a task that will be deregistered later', function (done) {
+
+    seneca.ready(function () {
+      seneca.act({
+        role: role,
+        cmd: 'register',
+        when: moment().add(10, 'minutes').toDate(),
+        task: delayedTask
+      }, function (err,data) {
+        should.not.exist(err);
+        debug('task registered:');
+        debug(data);
+        should.exist(data);
+        should.exist(data.jobId);
+        taskData = data;
+        done();
+      });
+    });
+  });
+
+  it('should deregister a task', function (done) {
+
+    seneca.ready(function () {
+      seneca.act({
+        role: role,
+        cmd: 'deregister',
+        jobId: taskData.jobId,
+        taskId: taskData.taskId
+      }, function (err, data) {
+        should.not.exist(err);
+        debug('deregister result:');
+        debug(data);
+        delayedTaskExecuted.should.equal(false);
+        done();
+      });
+    });
+  });
+
+  it('should register a task that will be updated later', function (done) {
+
+    seneca.ready(function () {
+      seneca.act({
+        role: role,
+        cmd: 'register',
+        when: moment().add(10, 'minutes').toDate(),
+        task: delayedTask
+      }, function (err,data) {
+        should.not.exist(err);
+        debug('task registered:');
+        debug(data);
+        should.exist(data);
+        should.exist(data.jobId);
+        taskData = data;
+        done();
+      });
+    });
+  });
+
+  it('should update a task to fire in 2 seconds and receive the notification', function (done) {
+    this.timeout(5000);
+    var callbackCount = 0;
+    var firedTask = function() {
+      if(++callbackCount == 2) {
         done();
       }
-    }, function (err) {
-      registeredAt = Date.now()
-      expect(err).to.be.null;
+    };
+    seneca.ready(function () {
+      seneca.act({
+        role: role,
+        cmd: 'update',
+        when: moment().add(2, 'seconds').toDate(),
+        task: firedTask,
+        jobId: taskData.jobId,
+        taskId: taskData.taskId
+      }, function (err,data) {
+        should.not.exist(err);
+        debug('task updated:');
+        debug(data);
+        should.exist(data);
+        should.exist(data.jobId);
+        taskData = data;
+        if(++callbackCount == 2) {
+          done();
+        }
+      });
     });
   });
 
