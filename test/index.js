3be8987cbb80472ef56c8fcf06b505f768d6d79b
Guy Ellis <wildfiction@gmail.com>
2014-09-22 11:34:52 -0700

    Refactored for general event listener.

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
 var seneca = require('seneca')();
 var moment = require('moment');
-var expect = require('chai').expect;
 var should = require('chai').should();
-var debug = require('debug')('test:seneca-quartz-scheduler');
+var debug = require('debug')('seneca-quartz-scheduler:test');
+var uuid = require('uuid');
 
 var config = {
   // When testing, change the URL to where your scheduler is running
@@ -13,47 +13,83 @@ var role = 'scheduler';
 
 seneca.use('../lib/scheduler.js', config);
 
-describe('quartz events', function(){
+describe('quartz events', function() {
+
+  var testCallbacks = {};
+
+  // We add the scheduler/event "listener" to receive scheduled
+  // jobs from the node-quartz-scheduler.
+  // In order to be able to run multiple tests and not add multiple
+  // of these "listeners" (which we could and then arbitrate among
+  // them) we are going to pass a UUID as a funcId into
+  // the data object that is passed through the scheduler and use
+  // that to index into a function setup by each test.
+  // This is similar to how you might use it in production code
+  // if you want to listen to multiple types of scheduled jobs
+  // except you probably wouldn't use functions but rather other
+  // seneca micro services as targets.
+  seneca.add({role:role, cmd:'event'}, function(args, callback){
+    debug('event received, args follow:');
+    debug(args);
+    if(!testCallbacks[args.data.funcId]) {
+      throw Error('Unfound function against this id: ' + args.data.funcId);
+    }
+    testCallbacks[args.data.funcId](args);
+    return this.prior(args,callback);
+  });
 
   it('should register a task and have it execute a second later', function (done) {
     var registeredAt;
+    var callbackCount = 0;
+    var funcId = uuid.v4();
+
+    testCallbacks[funcId] = function(args){
+      var now = Date.now();
+      now.should.be.above(registeredAt);
+      args.data.one.should.equal('#1');
+      args.data.two.should.equal('#2');
+      args.name.should.equal('Test Name #1');
+      if(++callbackCount == 2) {
+        done();
+      }
+    };
 
     seneca.ready(function () {
       seneca.act({
         role: role,
         cmd: 'register',
         when: moment().add(1, 'seconds').toDate(),
-        task: function (data) {
-          debug('task executed:');
-          debug(data);
-          var now = Date.now();
-          expect(now).to.be.above(registeredAt);
-          done();
-        },
-        random: {gold: 'color'}
+        name: 'Test Name #1',
+        data: {one:'#1',two: '#2', funcId: funcId}
       }, function (err, data) {
         debug('Registered task:');
         debug(data);
         registeredAt = Date.now();
-        expect(err).to.be.null;
+        should.not.exist(err);
+        if(++callbackCount == 2) {
+          done();
+        }
       });
     });
   });
 
   var taskData;
-  var delayedTaskExecuted = false;
-  var delayedTask = function() {
-    delayedTaskExecuted = true;
-  };
 
   it('should register a task that will be deregistered later', function (done) {
+    var funcId = uuid.v4();
+
+    testCallbacks[funcId] = function(){
+      // This should never be called because we're going to deregister it before it
+      // is called so throw an error if it's called.
+      throw Error('This function should not have been called.');
+    };
 
     seneca.ready(function () {
       seneca.act({
         role: role,
         cmd: 'register',
         when: moment().add(10, 'minutes').toDate(),
-        task: delayedTask
+        data: {some: 'random data', funcId: funcId}
       }, function (err,data) {
         should.not.exist(err);
         debug('task registered:');
@@ -72,26 +108,34 @@ describe('quartz events', function(){
       seneca.act({
         role: role,
         cmd: 'deregister',
-        jobId: taskData.jobId,
-        taskId: taskData.taskId
+        jobId: taskData.jobId
       }, function (err, data) {
         should.not.exist(err);
         debug('deregister result:');
+        data = JSON.parse(data);
         debug(data);
-        delayedTaskExecuted.should.equal(false);
+        data.key.should.equal('true');
         done();
       });
     });
   });
 
-  it('should register a task that will be updated later', function (done) {
+  it('should register a task that will be updated in next test', function (done) {
+    var funcId = uuid.v4();
+
+    testCallbacks[funcId] = function(){
+      // This should never be called because we're going to update it before it
+      // is called so throw an error if it's called and give it a different
+      // identifier in its payload
+      throw Error('This function should not have been called.');
+    };
 
     seneca.ready(function () {
       seneca.act({
         role: role,
         cmd: 'register',
         when: moment().add(10, 'minutes').toDate(),
-        task: delayedTask
+        data: {funcId: funcId, payload_number: '#1'}
       }, function (err,data) {
         should.not.exist(err);
         debug('task registered:');
@@ -107,19 +151,22 @@ describe('quartz events', function(){
   it('should update a task to fire in 2 seconds and receive the notification', function (done) {
     this.timeout(5000);
     var callbackCount = 0;
-    var firedTask = function() {
+    var funcId = uuid.v4();
+
+    testCallbacks[funcId] = function(args){
+      args.data.payload_number.should.equal('#2');
       if(++callbackCount == 2) {
         done();
       }
     };
+
     seneca.ready(function () {
       seneca.act({
         role: role,
         cmd: 'update',
         when: moment().add(2, 'seconds').toDate(),
-        task: firedTask,
         jobId: taskData.jobId,
-        taskId: taskData.taskId
+        data: {funcId: funcId, payload_number: '#2'}
       }, function (err,data) {
         should.not.exist(err);
         debug('task updated:');
